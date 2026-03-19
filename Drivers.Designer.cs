e790b003b64e3af856f73e3996e08429da1d7a9e
jazibasad <jazibasad.edusci2004@gmail.com>
2026-03-19 12:31:41 +0500

    last commit

--- Drivers.Designer.cs
+++ Drivers.Designer.cs
@@ -82,6 +82,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label1.Location = new System.Drawing.Point(24, 84);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 32);
