14eb856fda45f8a6d6f8c199c4fda55c4ec33eac
jazibasad <jazibasad.edusci2004@gmail.com>
2026-03-28 13:11:38 +0500

    last commit

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -66,7 +66,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Bookman Old Style", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label8.Location = new System.Drawing.Point(308, 377);
+            this.label8.Location = new System.Drawing.Point(324, 377);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(175, 35);
             this.label8.TabIndex = 8;
